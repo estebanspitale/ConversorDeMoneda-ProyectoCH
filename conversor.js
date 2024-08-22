c59babd23a5753d877f7ab40a04ff74d49688f76
Esteban Spitale <estebanspitale089@gmail.com>
2024-08-22 15:24:29 -0300

    proyectoFinal

--- conversor.js
+++ conversor.js
@@ -1,11 +1,11 @@
 // Programa consiste en cambiar pesos Argentinos a otra amplia variedad de monedas
 // Se solicita al usuario la cantidad de pesos que desea convertir, se hace el cambio y luego se agradece 
 // la visita. 
-// Tiene contemplado el uso de mayusculas o minusculas y decimales.
 
 
 document.addEventListener('DOMContentLoaded', () => {
     let tasasDeCambio = {};
+    let monedasDisponibles = [];
 
     async function cargarTasasDeCambio() {
         const tasasDeCambioEnLocalStorage = localStorage.getItem('tasasDeCambio');
@@ -20,10 +20,18 @@ document.addEventListener('DOMContentLoaded', () => {
                 tasasDeCambio = await response.json();
                 guardarDatos();  // Guarda el JSON en localStorage para futuras sesiones
             } catch (error) {
-                console.error('Error cargando tasas de cambio:', error);
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: 'Error cargando tasas de cambio: ' + error.message
+                });
             }
         }
+    
+        // Llamar a la función para mostrar las monedas disponibles
+        mostrarMonedasDisponibles();
     }
+    
 
     function guardarDatos() {
         const tasasDeCambioJSON = JSON.stringify(tasasDeCambio);
@@ -38,21 +46,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function manejarFormulario(event) {
         event.preventDefault();
-
+    
         const pesosArgentinos = parseFloat(document.getElementById('pesosArgentinos').value);
         const monedaDestino = document.getElementById('monedaDestino').value;
-        const resultElement = document.getElementById('result');
-
+    
         if (isNaN(pesosArgentinos) || pesosArgentinos <= 0) {
-            resultElement.textContent = 'Por favor, ingrese una cantidad válida de pesos argentinos.';
+            Swal.fire({
+                icon: 'warning',
+                title: 'Entrada inválida',
+                text: 'Por favor, ingrese una cantidad válida de pesos argentinos.'
+            });
             return;
         }
-
+    
         if (Object.keys(tasasDeCambio).includes(monedaDestino)) {
             const resultadoConversion = convertirMoneda(monedaDestino, pesosArgentinos);
-            resultElement.textContent = `${pesosArgentinos} pesos argentinos son aproximadamente ${resultadoConversion.toFixed(2)} ${monedaDestino.toUpperCase()}.`;
+            Swal.fire({
+                icon: 'success',
+                title: 'Resultado de la conversión',
+                text: `${pesosArgentinos} pesos argentinos son aproximadamente ${resultadoConversion.toFixed(2)} ${monedaDestino.toUpperCase()}.`
+            });
         } else {
-            resultElement.textContent = 'Moneda ingresada no válida.';
+            Swal.fire({
+                icon: 'error',
+                title: 'Moneda no válida',
+                text: 'Moneda ingresada no válida.'
+            });
+        }
+    }    
+
+    function mostrarMonedasDisponibles() {
+        const monedasElement = document.getElementById('monedasDisponibles');
+        if (monedasElement) {
+            monedasDisponibles = Object.keys(tasasDeCambio).filter(moneda => tasasDeCambio[moneda] > 1);
+            monedasElement.innerHTML = `<ul>${monedasDisponibles.map(moneda => `<li>${moneda.toUpperCase()}</li>`).join('')}</ul>`;
         }
     }
 
